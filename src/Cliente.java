31051c47678993be029130f12ec67674d9092cdc
Hugo1409 <hugodanielpenha@hotmail.com>
2021-07-06 15:15:34 -0300

    Desenvolvendo a classe Cliente

---
+++ src/Cliente.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Cliente {
+	private String nome;
+	public String CPF;
+	public Conta conta;
+	private ArrayList <String> historico;
+	 
+ public Cliente(String nome, String CPF, Conta conta) {
+   this.nome=nome;
+   this.CPF=CPF;
+   this.conta=conta;
+   historico=new ArrayList <String>();
+  }
+  
+ public void adcionarnoHistorico(String operacao) {
+   historico.add(operacao);
+  }
+
+ public void retornarHistorico() {
+   for (String hist: historico) {
+     System.out.println(hist);
+    }
+  }
+}
